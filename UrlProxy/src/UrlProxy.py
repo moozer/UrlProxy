09412370ca5ba61bf3c0dea5a6ef93fb3dbb76ce
moozer <bigmoozer@gmail.com>
2013-03-14 22:58:23 +0100

    Added redirect to test GET parameters

--- UrlProxy/src/UrlProxy.py
+++ UrlProxy/src/UrlProxy.py
@@ -7,6 +7,8 @@ Created on 14 Mar 2013
 from flask import Flask
 from flask.templating import render_template
 from flask.helpers import url_for, send_from_directory
+from flask.globals import request
+from werkzeug.utils import redirect
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,7 +16,10 @@ def index():
     print url_for('static', filename='index.html')
     return send_from_directory(app.static_folder, 'index.html')
 
-
+@app.route("/ProcessRedirect")
+def ProcessRedirect(methods=['GET']): #     is implied
+    URL = request.args['urltoget']
+    return redirect(URL)
 
 if __name__ == "__main__":
     app.run()
